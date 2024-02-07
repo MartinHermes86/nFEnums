fed95e2acdf2c12a6fbe4bad6840229540145ee4
Martin Hermes <okapiposter@gmail.com>
2024-02-07 13:43:35 +0100

    added some people

--- src/main/java/co/github/martinhermes86/Main.java
+++ src/main/java/co/github/martinhermes86/Main.java
@@ -10,14 +10,18 @@ public class Main {
         PersonRepository personRepository = new PersonRepository();
         personRepository.add(new Person(1, "John", DaysOfWeek.MONDAY, Gender.MALE));
         personRepository.add(new Person(2, "Lisa", DaysOfWeek.FRIDAY, Gender.FEMALE));
-        personRepository.add(new Person(2, "Jane", DaysOfWeek.TUESDAY, Gender.FEMALE));
-        personRepository.add(new Person(3, "Kiki", DaysOfWeek.WEDNESDAY, Gender.DIVERSE));
+        personRepository.add(new Person(3, "Jane", DaysOfWeek.TUESDAY, Gender.FEMALE));
+        personRepository.add(new Person(4, "Kiki", DaysOfWeek.WEDNESDAY, Gender.DIVERSE));
+        personRepository.add(new Person(5, "Koko", DaysOfWeek.THURSDAY, Gender.DIVERSE));
+        personRepository.add(new Person(6, "Manfred", DaysOfWeek.SATURDAY, Gender.MALE));
 
         System.out.println(personRepository.findPersonById(1));
-        System.out.println(personRepository.findPersonById(4));
+        System.out.println(personRepository.findPersonById(6));
+        System.out.println(personRepository.findPersonById(8));
 
         personRepository.countPersonsByGenders();
 
         personRepository.findPersonsByFavoriteDay(DaysOfWeek.TUESDAY).forEach(System.out::println);
+        personRepository.findPersonsByFavoriteDay(DaysOfWeek.SATURDAY).forEach(System.out::println);
     }
 }
